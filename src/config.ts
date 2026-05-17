e1832481c50cc2b86ec0cdf2e15afb3ddcbfb1f5
jiaezzang <visveryver2@gmail.com>
2026-05-17 23:48:36 +0900

    chore: 컨테츠 업데이트(컴퓨터 입력장치 알기2)

--- src/config.ts
+++ src/config.ts
@@ -10,5 +10,11 @@ export const LINKS = [
     description: "마우스와 키보드 사용법을 익혀요.",
     href: "https://computer-input-basics.vercel.app/",
     label: "1-2학년"
+  },
+  {
+    title: "컴퓨터 입력장치 알기 2",
+    description: "키보드 입력 연습을 해봐요.",
+    href: "https://computer-input-practice.vercel.app/",
+    label: "1-2학년"
   }
 ];
